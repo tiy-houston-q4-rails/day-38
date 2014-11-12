65b0ec67cfc95c1fbe0317b2bae2f978223d0bfb
Jesse Wolgamott <jesse@comalproductions.com>
2014-11-12 11:29:57 -0600

    POSTing json into create

--- cars/app/controllers/api/users_controller.rb
+++ cars/app/controllers/api/users_controller.rb
@@ -1,7 +1,15 @@
 class Api::UsersController < ApplicationController
 
+  protect_from_forgery with: :null_session
+
   def index
     @users = User.all
     render json: @users
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name))
+    @user.save
+    render json: @user
   end
 end
--- cars/config/routes.rb
+++ cars/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "users" => 'users#index'
+    post "/users"=> 'users#create'
     get 'cars' => 'cars#index'
   end
 
